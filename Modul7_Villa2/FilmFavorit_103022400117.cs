cd77911ac325543691a1761a3b8a2bd77ce88d89
ilhamrmdhn <ilham712svr@gmail.com>
2026-04-14 07:23:37 +0700

    menambahkan class FilmFavorit_103022400117

---
+++ Modul7_Villa2/FilmFavorit_103022400117.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class FilmFavorit_103022400117
+{
+    public string? title { get; set; }
+    public string? director { get; set; }
+    public string? year { get; set; }
+    public string? genre { get; set; }
+    public double? rating { get; set; }
+    public int? durationMinutes { get; set; }
+    public bool? isWatched { get; set; }
+}
