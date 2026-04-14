87ae2e322abc87c33315bcc8c911bcf2cc73cdb4
4uLmost <muhammaddhiyaul273@gmail.com>
2026-04-14 08:09:25 +0700

    menambahkan class WatchList

--- Modul7_Villa2/Program.cs
+++ Modul7_Villa2/Program.cs
@@ -11,8 +11,8 @@ public class Program
             var options = new JsonSerializerOptions { IncludeFields = true };
 
             Console.WriteLine("\n=== Film Favorit Aul ===");
-            string jsonaul = File.ReadAllText("jurnal7_1_103022400046.json");
-            var favaul = JsonSerializer.Deserialize<FilmFaforit_103022400046>(jsonaul, options);
+            string jsonaul1 = File.ReadAllText("jurnal7_1_103022400046.json");
+            var favaul = JsonSerializer.Deserialize<FilmFaforit_103022400046>(jsonaul1, options);
             Console.WriteLine($"Judul Film {favaul?.title} ");
             Console.WriteLine($"Direktur Film {favaul?.director} ");
             Console.WriteLine($"Tahun Film {favaul?.year} ");
@@ -21,11 +21,22 @@ public class Program
             Console.WriteLine($"Durasi Film {favaul?.durationMinute} ");
             Console.WriteLine($"Apakah Film ini sudah ditonton{favaul?.isWatched} ");
 
-
+            string jsonaul2 = File.ReadAllText("jurnal7_2_103022400046.json");
+            var watchList = JsonSerializer.Deserialize<WatchList_103022400046>(jsonaul2, options);
+            Console.WriteLine($"Watchlist name: {watchList?.watchlistName} ");
+            Console.WriteLine($"Created By: {watchList?.createdBy} ");
+            Console.WriteLine("Watchlist:");
+            if (watchList?.movies != null)
+            {
+                for (int i = 0; i < watchList.movies.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1} {watchList.movies[i].id} {watchList.movies[i].title} ( {watchList.movies[i].year} - {watchList.movies[i].rating} )");
+                }
+            }
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error pada data Dewo: {ex.Message}");
+            Console.WriteLine($"Error pada data Aul: {ex.Message}");
         }
     }
     public static void Main()
---
+++ Modul7_Villa2/WatchList_103022400046.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+public class WatchList_103022400046
+{
+    public string? watchlistName { get; set; }
+    public string? createdBy { get; set; }
+
+    public List<List_103022400046>? movies { get; set; }
+}
+
+public class List_103022400046
+{
+    public string? id { get; set; }
+    public string? title { get; set; }
+    public int? year { get; set; }
+    public string? genre { get; set; }
+    public double? rating { get; set; }
+}
