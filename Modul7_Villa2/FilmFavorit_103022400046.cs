4d4fd5bcca647a32bec49a247be66416318d9919
4uLmost <muhammaddhiyaul273@gmail.com>
2026-04-14 07:53:14 +0700

    menambahkan class FilmFavorit

---
+++ Modul7_Villa2/FilmFavorit_103022400046.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+
+class FilmFaforit_103022400046
+{
+    public string? title { get; set; }
+    public string? director { get; set; }
+    public string? year { get; set; }
+    public string? genre { get; set; }
+    public double? rating { get; set; }
+    public int? durationMinute { get; set; }
+    public bool? isWatched { get; set; }
+}
--- Modul7_Villa2/Program.cs
+++ Modul7_Villa2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class Program
+{
+    public static void ReadJSON_103022400046()
+    {
+        try
+        {
+            var options = new JsonSerializerOptions { IncludeFields = true };
+
+            Console.WriteLine("\n=== Film Favorit Aul ===");
+            string jsonaul = File.ReadAllText("jurnal7_1_103022400046.json");
+            var favaul = JsonSerializer.Deserialize<FilmFaforit_103022400046>(jsonaul, options);
+            Console.WriteLine($"Judul Film {favaul?.title} ");
+            Console.WriteLine($"Direktur Film {favaul?.director} ");
+            Console.WriteLine($"Tahun Film {favaul?.year} ");
+            Console.WriteLine($"Genre Film {favaul?.genre} ");
+            Console.WriteLine($"Rating Film {favaul?.rating} ");
+            Console.WriteLine($"Durasi Film {favaul?.durationMinute} ");
+            Console.WriteLine($"Apakah Film ini sudah ditonton{favaul?.isWatched} ");
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error pada data Dewo: {ex.Message}");
+        }
+    }
+    public static void Main()
+    {
+        ReadJSON_103022400046();
+    }
+}
